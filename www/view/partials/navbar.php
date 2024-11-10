6c1110ce2ccccaff464191e8945d40636b0ae7bd
= <pablo.dorrio.vazquez@gmail.com>
2024-11-10 18:32:21 +0100

    feat: segment views

---
+++ www/view/partials/navbar.php
@@ -0,0 +1,19 @@
+<?php
+// file: view/partials/navbar.php
+?>
+<link rel="stylesheet" type="text/css" href="../../assets/styles/navbar.css">
+
+<nav class="navbar">
+    <img class="navbar-logo" src="../../assets/images/logo.png" alt="logo">
+    <ul class="navbar-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="index.php?controller=users&amp;action=register">Register</a></li>
+        <li><a href="index.php?controller=users&amp;action=login">Login</li>
+        <li><a href="#">Profile</a></li>
+    </ul>
+    <div class="navbar-toggle">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+</nav>
--- www/view/users/login.php
+++ www/view/users/login.php
@@ -1,22 +1,50 @@
 <?php
-//file: view/users/login.php
+// file: view/users/login.php
 
-require_once(__DIR__."/../../config/ViewManager.php");
+require_once(__DIR__ . "/../../config/ViewManager.php");
 $view = ViewManager::getInstance();
 $view->setVariable("title", "Login");
 $errors = $view->getVariable("errors");
 ?>
 
-<h1><?= i18n("Login") ?></h1>
-<?= isset($errors["general"])?$errors["general"]:"" ?>
+<link rel="stylesheet" type="text/css" href="../../assets/styles/register.css">
 
-<form action="index.php?controller=users&amp;action=login" method="POST">
-	<?= i18n("Username")?>: <input type="text" name="username">
-	<?= i18n("Password")?>: <input type="password" name="passwd">
-	<input type="submit" value="<?= i18n("Login") ?>">
-</form>
+<div class="register-section">
+    <div class="register-form-container">
+        <div class="form-icon">
+            <img src="../../assets/images/isotype.png" alt="icon">
+        </div>
+        <form class="register-form" method="POST" action="index.php?controller=users&amp;action=login">
+            <div class="form-group">
+                <label for="email"><?= i18n("Username") ?></label>
+                <input type="text" id="username" name="username" required>
+                <span class="error-message"><?= isset($errors["username"]) ? i18n($errors["username"]) : "" ?></span>
+            </div>
 
-<p><?= i18n("Not user?")?> <a href="index.php?controller=users&amp;action=register"><?= i18n("Register here!")?></a></p>
-<?php $view->moveToFragment("css");?>
-<link rel="stylesheet" type="text/css" src="assets/styles/style2.css">
-<?php $view->moveToDefaultFragment(); ?>
+            <div class="form-group">
+                <label for="password"><?= i18n("Password") ?></label>
+                <input type="password" id="password" name="passwd" required>
+                <span class="error-message"><?= isset($errors["passwd"]) ? i18n($errors["passwd"]) : "" ?></span>
+            </div>
+
+            <div class="form-group show-password">
+                <input type="checkbox" id="show-password" onclick="togglePasswordVisibility()">
+                <label for="show-password"><?= i18n("Show password") ?></label>
+            </div>
+
+            <div class="form-actions">
+                <button type="button" class="btn-back" onclick="window.location.href='?controller=user&amp;action=register'"><?= i18n("Back") ?></button>
+                <button type="submit" class="btn-register"><?= i18n("Login") ?></button>
+            </div>
+        </form>
+
+        <p class="alternative-action"><?= i18n("Not a user?") ?> <a href="index.php?controller=users&amp;action=register"><?= i18n("Register here!") ?></a></p>
+    </div>
+</div>
+
+<script>
+    function togglePasswordVisibility() {
+        var passwordInput = document.getElementById("password");
+        passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+    }
+</script>
