78759307426b5fc4b04f62b5cc1fb9aade94cab1
= <pablo.dorrio.vazquez@gmail.com>
2024-11-11 12:50:42 +0100

    feat: add styles to add.php

--- www/view/groups/add.php
+++ www/view/groups/add.php
@@ -11,31 +11,40 @@ $view->setVariable("title", "Add Group");
 
 ?>
 
-<h1><?= i18n("Create group")?></h1>
-<form action="index.php?controller=groups&amp;action=add" method="POST" id="group-form">
-    <?= i18n("Name") ?>: <input type="text" name="name" value="<?= $group->getName() ?>">
-    <?= isset($errors["name"]) ? i18n($errors["name"]) : "" ?><br>
+<link rel="stylesheet" href="../../assets/styles/groups/add.css" type="text/css">
 
-    <?= i18n("Description") ?>: <br>
-    <textarea name="description" rows="4" cols="50"><?= htmlentities($group->getDescription()) ?></textarea>
-    <?= isset($errors["description"]) ? i18n($errors["description"]) : "" ?><br>
-
-    <!-- Section for members -->
-    <label for="members"><?= i18n("Participants") ?>:</label><br>
-
-    <!-- Container for dynamic participant input fields -->
-    <div id="members-container">
-        <div class="member-input">
-            <input type="text" name="members[]" value="<?= htmlentities($username) ?>" readonly/>
-        </div>
+<div class="main">
+    <div class="top-icon">
+        <img src="../../assets/images/isotype.png" alt="Groups Icon">
     </div>
-    
-    <button type="button" id="add-participant"><?= i18n('Add Participant') ?></button><br>
 
-    <?= isset($errors["members"]) ? i18n($errors["members"]) : "" ?><br>
+    <h1 class="main-title"><?= i18n("Create group")?></h1>
 
-    <input type="submit" name="submit" value="<?= i18n("Create Group") ?>">
-</form>
+    <form action="index.php?controller=groups&amp;action=add" method="POST" id="group-form">
+        <?= i18n("Name") ?>: <input type="text" name="name" value="<?= $group->getName() ?>">
+        <?= isset($errors["name"]) ? i18n($errors["name"]) : "" ?><br>
+
+        <?= i18n("Description") ?>: <br>
+        <textarea name="description" rows="4" cols="50"><?= htmlentities($group->getDescription()) ?></textarea>
+        <?= isset($errors["description"]) ? i18n($errors["description"]) : "" ?><br>
+
+        <!-- Section for members -->
+        <label for="members"><?= i18n("Participants") ?>:</label><br>
+
+        <!-- Container for dynamic participant input fields -->
+        <div id="members-container">
+            <div class="member-input">
+                <input type="text" name="members[]" value="<?= htmlentities($username) ?>" readonly/>
+            </div>
+        </div>
+        
+        <button type="button" id="add-participant"><?= i18n('Add Participant') ?></button><br>
+
+        <?= isset($errors["members"]) ? i18n($errors["members"]) : "" ?><br>
+
+        <input type="submit" name="submit" value="<?= i18n("Create Group") ?>">
+    </form>
+</div>
 
 <!-- JavaScript to add and remove participants -->
 <script>
