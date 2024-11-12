dd27f225c78d4ac6a98783762c8b72cd3fa62cce
David Alvarez <alvareziglesiasdavid03@gmail.com>
2024-11-12 23:44:04 +0100

    button movements style

--- www/view/groups/view.php
+++ www/view/groups/view.php
@@ -70,7 +70,7 @@ $view->setVariable("title", "View Group");
                 <?php endforeach; ?>
             </ul>
 
-            <div class="suggested-movements">
+            <div class="add-">
                 <a href="index.php?controller=groups&amp;action=movements&amp;id=<?= $group->getId() ?>" class="suggested-movements-button">
                     <?= i18n("View Suggested Movements") ?>
                 </a>
